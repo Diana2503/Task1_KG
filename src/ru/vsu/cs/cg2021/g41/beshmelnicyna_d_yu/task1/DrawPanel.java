4488100921d221cd0425a8c5c705694bdd8abb78
Diana Besxmelnicyna <dbesxmelnicyna@mail.ru>
2021-10-03 23:45:31 +0300

    task1

--- src/ru/vsu/cs/cg2021/g41/beshmelnicyna_d_yu/task1/DrawPanel.java
+++ src/ru/vsu/cs/cg2021/g41/beshmelnicyna_d_yu/task1/DrawPanel.java
@@ -11,12 +11,12 @@ public class DrawPanel extends JPanel {
         world = new World(0, 0, 0, 0, Color.CYAN);
         world.addDrawingObject(new Sky(0, 0, 1, 1));
         world.addDrawingObject(new Sun(0.3, 0.3, 0.2, new Color(0xFADC03)));
-        world.addDrawingObject(new Mushrooms(0.24, 0.886, Color.GRAY, Color.RED));
-        world.addDrawingObject(new Mushrooms(0.8, 0.8286, Color.GRAY, Color.RED));
-        world.addDrawingObject(new House(0.38, 0.4857, 0.25, 0.357, Color.RED, Color.ORANGE));
-        world.addDrawingObject(new Cloud(0.37, 0.0714, Color.WHITE, Color.CYAN, Color.RED));
-
-
+        world.addDrawingObject(new Mushrooms(0.24, 0.886, new Color(0x3A3A3D), new Color(0xA10505)));
+        world.addDrawingObject(new Mushrooms(0.8, 0.8286, new Color(0x3A3A3D), new Color(0xA10505)));
+        world.addDrawingObject(new House(0.38, 0.4857, 0.25, 0.357, new Color(0xE31515), new Color(0xF3841B)));
+        world.addDrawingObject(new Cloud(0.37, 0.0714, new Color(0xD9D9F8), new Color(0x0E99E8), new Color(0xEE38BF)));
+        world.addDrawingObject(new Cedar(0.1, 0.743, 0.4286, new Color(0x0D770D)));
+        world.addDrawingObject(new Balloon(0.7, 0.0714, 0.2, 0.2857, new Color(0xA214A2), new Color(0x4D4D96)));
     }
 
     @Override
