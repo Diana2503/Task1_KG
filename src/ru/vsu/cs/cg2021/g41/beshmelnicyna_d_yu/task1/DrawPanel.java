d40b4994565acc0ee479d636bd44d3256357704b
Diana Besxmelnicyna <dbesxmelnicyna@mail.ru>
2021-10-19 21:13:01 +0300

    task1

--- src/ru/vsu/cs/cg2021/g41/beshmelnicyna_d_yu/task1/DrawPanel.java
+++ src/ru/vsu/cs/cg2021/g41/beshmelnicyna_d_yu/task1/DrawPanel.java
@@ -2,6 +2,9 @@ package ru.vsu.cs.cg2021.g41.beshmelnicyna_d_yu.task1;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Random;
 
 public class DrawPanel extends JPanel {
@@ -9,7 +12,26 @@ public class DrawPanel extends JPanel {
 
     public DrawPanel() {
         Random rnd = new Random();
+        List<Mushrooms> mushrooms = new ArrayList<>();
+
+        for (int i = 0; i < 50; i++) {
+            mushrooms.add(new Mushrooms(rnd.nextDouble() * 0.3, rnd.nextDouble() * 0.5 + 0.6, new Color(0x6E6E79), new Color(0xA10505)));
+        }
+        for (int i = 0; i < 50; i++) {
+            mushrooms.add(new Mushrooms(rnd.nextDouble() * 0.5 + 0.65, rnd.nextDouble() * 0.5 + 0.6, new Color(0x6E6E79), new Color(0xA10505)));
+        }
+
+        mushrooms.sort(new Comparator<Mushrooms>() {
+            @Override
+            public int compare(Mushrooms o1, Mushrooms o2) {
+                return (int) (o1.getY() - o2.getY());
+            }
+        });
+
         world = new World(0, 0, 0, 0, Color.CYAN);
+        for (Mushrooms mushroom : mushrooms) {
+            world.addDrawingObject(mushroom);
+        }
         world.addDrawingObject(new Sky(0, 0, 1, 1, Color.CYAN));
         world.addDrawingObject(new Sun(0.2, 0.25, 0.05, new Color(0xFADC03)));
         world.addDrawingObject(new Background(0, 0.6, 1, 0.4));
@@ -17,12 +39,6 @@ public class DrawPanel extends JPanel {
         world.addDrawingObject(new Cloud(0.37, 0.0714, new Color(0xD9D9F8), new Color(0x0E99E8), new Color(0xEE38BF)));
         world.addDrawingObject(new Balloon(0.7, 0.0714, 0.2, 0.2857, new Color(0xA214A2), new Color(0x4D4D96)));
 
-        for (int i = 0; i < 5; i++) {
-            world.addDrawingObject(new Mushrooms(rnd.nextDouble() * 0.3, rnd.nextDouble() * 0.5 + 0.6, new Color(0x6E6E79), new Color(0xA10505)));
-        }
-        for (int i = 0; i < 5; i++) {
-            world.addDrawingObject(new Mushrooms(rnd.nextDouble() * 0.5 + 0.65, rnd.nextDouble() * 0.5 + 0.6, new Color(0x6E6E79), new Color(0xA10505)));
-        }
     }
 
     @Override
--- src/ru/vsu/cs/cg2021/g41/beshmelnicyna_d_yu/task1/Mushrooms.java
+++ src/ru/vsu/cs/cg2021/g41/beshmelnicyna_d_yu/task1/Mushrooms.java
@@ -5,11 +5,17 @@ import java.awt.*;
 public class Mushrooms extends DrawingObject {
     private final Color color1;
     private final Color color2;
+    private final Double y;
+
+    public Double getY() {
+        return y;
+    }
 
     public Mushrooms(double cofX, double cofY, Color color1, Color color2) {
         super(cofX, cofY);
         this.color1 = color1;
         this.color2 = color2;
+        this.y = cofY;
     }
 
     @Override
@@ -19,11 +25,17 @@ public class Mushrooms extends DrawingObject {
 
         g.setStroke(new BasicStroke(25));
         g.setColor(color1);
-        g.drawLine((int) (cofX * panelWidth), (int) (cofY * panelHeight), (int) (cofX * panelWidth),
-                (int) ((cofY * panelHeight) - width));
+        //g.drawLine((int) (cofX * panelWidth), (int) (cofY * panelHeight), (int) (cofX * panelWidth),
+        //        (int) ((cofY * panelHeight) - width));
+        g.fillRect((int) (cofX * panelWidth), (int) (cofY * panelHeight), width,
+                width);
+        g.setStroke(new BasicStroke(2));
+        g.setColor(new Color(1));
+        g.drawRect((int) (cofX * panelWidth), (int) (cofY * panelHeight), width,
+                width);
 
         g.setColor(color2);
-        g.fillArc(((int) ((cofX - 0.035) * panelWidth)), ((int) ((cofY - 0.0928) * panelHeight)),
+        g.fillArc(((int) ((cofX - 0.017) * panelWidth)), ((int) ((cofY - 0.05) * panelHeight)),
                 width * 2, width * 2, 0, 180);
 
         g.setColor(oldColor);
